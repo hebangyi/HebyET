c976d352dac82be0511b97c1f62561726a900341
hebangyi <heby0007@gmail.com>
2025-05-08 16:48:08 +0800

    [框架] 框架调整

--- Unity/Resource/UI/FairyGUI/plugins/ETCodeGenerate/GenCode_CSharp.js
+++ Unity/Resource/UI/FairyGUI/plugins/ETCodeGenerate/GenCode_CSharp.js
@@ -36,7 +36,7 @@ function genComponent(handler) {
     let codeTemplatePath = "/ETCodeGenerate/template/Unity";
     let templatePath = Utils_1.default.FormatStr("{0}{1}", csharp_1.FairyEditor.App.pluginManager.projectPluginFolder, codeTemplatePath);
     //CollectClasses(stripeMemeber, stripeClass, fguiNamespace)
-    let classes = handler.CollectClasses(true, true, null);
+    let classes = handler.CollectClasses(true, false, null);
     handler.SetupCodeFolder(exportCodePath, "cs"); //check if target folder exists, and delete old files
     let getMemberByName = settings.getMemberByName;
     let classCnt = classes.Count;
@@ -74,7 +74,7 @@ function genComponentSystem(handler) {
     let templateFileName = "ETComponentSystem.template";
     let codeTemplatePath = "/ETCodeGenerate/template/Unity";
     let templatePath = Utils_1.default.FormatStr("{0}{1}", csharp_1.FairyEditor.App.pluginManager.projectPluginFolder, codeTemplatePath);
-    let classes = handler.CollectClasses(true, true, null);
+    let classes = handler.CollectClasses(true, false, null);
     let classCnt = classes.Count;
     let getMemberByName = settings.getMemberByName;
     for (let i = 0; i < classCnt; i++) {
