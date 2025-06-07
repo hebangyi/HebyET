e745361f99ba58de8d9e29a78e4821ab4254a77c
heby <563124577@qq.com>
2025-06-07 21:54:28 +0800

    [框架] 代码提交

--- Unity/Assets/Scripts/Loader/GlobalComponent.cs
+++ Unity/Assets/Scripts/Loader/GlobalComponent.cs
@@ -8,19 +8,33 @@ namespace ET
         [EntitySystem]
         public static void Awake(this GlobalComponent self)
         {
+            GlobalComponent.Instance = self;
+            
+            
             self.Global = GameObject.Find("/Global").transform;
+            self.MainCamera = GameObject.Find("/Global/MainCamera").GetComponent<Camera>();
+            // self.UICamera = GameObject.Find("/Global/UICamera").GetComponent<Camera>();
+            // self.UI = GameObject.Find("/Global/UI").transform;
             self.Unit = GameObject.Find("/Global/Unit").transform;
-            self.UI = GameObject.Find("/Global/UI").transform;
+            
             self.GlobalConfig = Resources.Load<GlobalConfig>("GlobalConfig");
         }
     }
-    
+
     [ComponentOf(typeof(Scene))]
-    public class GlobalComponent: Entity, IAwake
+    public class GlobalComponent : Entity, IAwake
     {
+        [StaticField]
+        public static GlobalComponent Instance;
+        
         public Transform Global;
         public Transform Unit { get; set; }
-        public Transform UI;
+        public Transform UI { get; set; }
+        public Camera UICamera { get; set; }
+        public Camera MainCamera { get; set; }
+        
+        
+        
 
         public GlobalConfig GlobalConfig { get; set; }
     }
