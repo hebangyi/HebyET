2041c2af4979cd7fcccfa9d966f28065822bc28a
heby <563124577@qq.com>
2025-06-09 06:39:06 +0800

    [资源] 资源加载

---
+++ Unity/Assets/Scripts/HotfixView/Client/Module/GameObjectPool/GameObjectPoolComponentSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ET.Client
+{
+    [EntitySystemOf(typeof(GameObjectPoolComponent))]
+    public static class GameObjectPoolComponentSystem
+    {
+        public static void AddLoadGameObject(this GameObjectPoolComponent self, string path, long formId)
+        {
+
+            /*var gameObjects = self.ExternalReferences.GetValueOrDefault(path);
+            if (gameObjects != null)
+            {
+                // TODO 缓存对象
+            }*/
+
+            GameObjectLoadContext context = new GameObjectLoadContext();
+            context.Path = path;
+            context.FormId = formId;
+            self.LoadingList.Add(context);
+
+        }
+        
+        
+        [EntitySystem]
+        private static void Update(this ET.Client.GameObjectPoolComponent self)
+        {
+            if (self.LoadingList.Count == 0)
+            {
+                return;
+            }
+
+            // TODO 控制加载频率 量
+            foreach (var item in self.LoadingList)
+            {
+                self.LodeGameObject(item).Coroutine();
+            }
+        }
+        
+        private static async ETTask LodeGameObject(this ET.Client.GameObjectPoolComponent self, GameObjectLoadContext context)
+        {
+            var unityScene = self.GetParent<UnityScene>();
+            var resourcesLoaderComponent = unityScene.GetComponent<ResourcesLoaderComponent>();
+            
+            
+            GameObject gameObject = await resourcesLoaderComponent.LoadAssetAsync<GameObject>(context.Path);
+            context.DoLoadFinish(gameObject);
+        }
+    }
+}
+
---
+++ Unity/Assets/Scripts/ModelView/Client/Module/GameObject/GameObjectPoolComponent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ET.Client
+{
+    [ChildOf(typeof(GameObjectPoolComponent))]
+    public class GameObjectLoadContext
+    {
+        public string Path;
+        public long FormId;
+        public Action<GameObject> DoLoadFinish;
+        
+        public struct GameObjectLoadHandler
+        {
+            public GameObject GameObject;
+            public long Id;
+        }
+        
+        public void LoadedFinish(GameObject gameObject) 
+        {
+            DoLoadFinish?.Invoke(gameObject);   
+        }
+    }
+    
+    
+    [ComponentOf(typeof(UnityScene))]
+    public class GameObjectPoolComponent:Entity, IUpdate
+    {
+        [StaticField]
+        public static GameObjectPoolComponent Instance;
+        public List<GameObjectLoadContext> LoadingList = new ();
+        public Dictionary<string, List<GameObject>> Pools = new ();
+    }
+}
