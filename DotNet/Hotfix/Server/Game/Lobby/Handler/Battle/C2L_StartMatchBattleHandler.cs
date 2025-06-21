b10b77a51b0e6fb0837d00576dd2ec090081ab3a
heby <563124577@qq.com>
2025-06-21 19:18:49 +0800

    [游戏框架] 设计改造 ECS

---
+++ DotNet/Hotfix/Server/Game/Lobby/Handler/Battle/C2L_StartMatchBattleHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace ET.Server;
+
+[MessageClientHandler(SceneType.Lobby)]
+public class C2L_StartMatchBattleHandler: MessageClientHandler<LobbyRole, C2L_StartMatchBattle, L2C_StartMatchBattle>
+{
+    protected override void Run(LobbyRole e, C2L_StartMatchBattle request, L2C_StartMatchBattle response)
+    {
+        
+    }
+}
