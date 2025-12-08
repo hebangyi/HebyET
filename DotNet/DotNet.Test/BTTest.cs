f17157a525378c3c38fb8743f4be7e4b42ea1f4f
heby <563124577@qq.com>
2025-12-08 17:44:20 +0800

    [功能提交] BT 行为树 接入与测试

---
+++ DotNet/DotNet.Test/BTTest.cs
@@ -0,0 +1,18 @@
+﻿namespace DotNet.Test;
+
+public static class BTTest
+{
+    public static void Test()
+    {
+        behaviac.Workspace.Instance.FileFormat = behaviac.Workspace.EFileFormat.EFF_cs;
+        AIAgent aiAgent = new AIAgent();
+        aiAgent.btload("AIAgent", false);
+        aiAgent.btsetcurrent("AIAgent");
+
+        for (int i = 0; i < 1000; i++)
+        {
+            var status = aiAgent.btexec();
+            Console.WriteLine($"status : {status} , aiAgent : {aiAgent.status}");
+        }
+    }
+}
