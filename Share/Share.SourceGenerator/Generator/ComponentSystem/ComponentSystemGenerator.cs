e6af836025fd232de88b5978c89b9cf98cc5de75
hebangyi <heby0007@gmail.com>
2025-12-24 18:01:45 +0800

    [框架] 工具调整

--- Share/Share.SourceGenerator/Generator/ComponentSystem/ComponentSystemGenerator.cs
+++ Share/Share.SourceGenerator/Generator/ComponentSystem/ComponentSystemGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using ET.Analyzer;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -10,11 +11,10 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 namespace ET.Generator;
 
 [Generator(LanguageNames.CSharp)]
-public class ComponentSystemGenerator: ISourceGenerator
+public class ComponentSystemGenerator : ISourceGenerator
 {
     public List<SolutionLoader> SolutionLoaders = new List<SolutionLoader>();
-    
-    
+
     public class SolutionLoader
     {
         public string SearchFolder;
@@ -23,21 +23,49 @@ public class ComponentSystemGenerator: ISourceGenerator
 
         public void Search()
         {
+            if (!Directory.Exists(SearchFolder))
+            {
+                return;
+            }
+
             var filePaths = Directory.GetFiles(SearchFolder, "*.cs", SearchOption.AllDirectories);
             foreach (var filePath in filePaths)
             {
+                // File.AppendAllText("C:\\Users\\Administrator\\Desktop\\abc.txt", $"File Class {filePath}, File Exists \n");
                 SearchFiles.Add(Path.GetFileNameWithoutExtension(filePath));
             }
         }
     }
-    
-    
+
     public const string Template = $$"""
-                                                   namespace {namespaceName}
-                                                   {
-                                                   }
-                                                   """;
+                                     namespace {namespaceName}
+                                     {
+                                        [FriendOf(typeof({componentType}))]
+                                        [EntitySystemOf(typeof({componentType}))]
+                                        public static partial class {componentType}System
+                                        {
+                                            {EntitySystem}
+                                        }
+                                     }
+                                     """;
+
+    public const string AwakeSystemTemplate = $$"""
+                                                 
+                                                        [EntitySystem]
+                                                        private static void Awake(this {componentType} self{param})
+                                                        {
+                                                        }
+                                                 """;
     
+    
+    public const string EntitySystemTemplate = $$"""
+                                                 
+                                                        [EntitySystem]
+                                                        private static void {lifeCycle}(this {componentType} self)
+                                                        {
+                                                        }
+                                                 """;
+
     public void Initialize(GeneratorInitializationContext context)
     {
         string rootPath = Path.GetFullPath("./");
@@ -50,27 +78,27 @@ public class ComponentSystemGenerator: ISourceGenerator
         clientModelViewLoader.SearchFolder = Path.Combine(rootPath, "Unity\\Assets\\Scripts\\ModelView\\Client");
         clientModelViewLoader.OutFolder = Path.Combine(rootPath, "Unity\\Assets\\Scripts\\HotfixView\\Client\\Module\\System");
         clientModelViewLoader.Search();
-        
+
         SolutionLoader shareModelLoader = new SolutionLoader();
         shareModelLoader.SearchFolder = Path.Combine(rootPath, "Unity\\Assets\\Scripts\\Model\\Share");
         shareModelLoader.OutFolder = Path.Combine(rootPath, "Unity\\Assets\\Scripts\\Hotfix\\Share\\Module\\System");
         shareModelLoader.Search();
-        
+
         SolutionLoader clientModelLoader = new SolutionLoader();
         clientModelLoader.SearchFolder = Path.Combine(rootPath, "Unity\\Assets\\Scripts\\Model\\Client");
         clientModelLoader.OutFolder = Path.Combine(rootPath, "Unity\\Assets\\Scripts\\Hotfix\\Client\\Module\\System");
         clientModelLoader.Search();
-        
+
         SolutionLoader serverModelLoader = new SolutionLoader();
         serverModelLoader.SearchFolder = Path.Combine(rootPath, "DotNet\\Model\\Server");
         serverModelLoader.OutFolder = Path.Combine(rootPath, "DotNet\\Hotfix\\Server\\Module\\System");
         serverModelLoader.Search();
-        
+
         SolutionLoaders.Add(clientModelViewLoader);
         SolutionLoaders.Add(shareModelLoader);
         SolutionLoaders.Add(clientModelLoader);
         SolutionLoaders.Add(serverModelLoader);
-        
+
         context.RegisterForSyntaxNotifications(() => ComponentClassReceiver.Create());
     }
 
@@ -80,12 +108,12 @@ public class ComponentSystemGenerator: ISourceGenerator
         {
             return;
         }
-        
+
         if (receiver.CompnentTypes.Count == 0)
         {
             return;
         }
-        
+
         foreach (var type in receiver.CompnentTypes)
         {
             this.GenerateFiles(type, context, receiver);
@@ -118,26 +146,79 @@ public class ComponentSystemGenerator: ISourceGenerator
         {
             Directory.CreateDirectory(solutionLoader.OutFolder);
         }
-        
-        string fileName = $"{className}Handler.cs";
+
+        string fileName = $"{className}System.cs";
+
         var filePath = Path.Combine(solutionLoader.OutFolder, fileName);
+        
         if (File.Exists(filePath))
         {
             return;
         }
-        
+
         string template = Template;
-        string namespaceName = "ET";
+        string namespaceName = namespaceSymbol.ToString();
+
         var code = template.Replace("{namespaceName}", namespaceName);
-        code = code.Replace("{className}", className);
+        code = code.Replace("{componentType}", className);
+
+
+        StringBuilder methodBuilder = new StringBuilder();
+        foreach (var type in classDeclarationSyntax.BaseList.Types)
+        {
+            if (type.ToString().Contains("IAwake"))
+            {
+                var typeStr = type.ToString();
+                int leftBracketIndex = typeStr.IndexOf('<');
+                int rightBracketIndex = typeStr.LastIndexOf('>');
+                
+                if (leftBracketIndex == -1 || rightBracketIndex == -1 || rightBracketIndex < leftBracketIndex)
+                {
+                    string part = AwakeSystemTemplate.Replace("{componentType}", className).Replace("{param}", "");
+                    methodBuilder.Append(part);
+                    methodBuilder.AppendLine();
+                }
+                else
+                {
+                    string contentInsideBrackets = typeStr.Substring(leftBracketIndex + 1, rightBracketIndex - leftBracketIndex - 1);
+                    var paramTypes = contentInsideBrackets.Split(',');
+
+                    string param = "";
+                    for (int i = 0 ; i < paramTypes.Length ; i++)
+                    {
+                        param = ", ";
+                        param += $"{paramTypes[i]} p{i + 1}";
+                    }
+                    
+                    var part = AwakeSystemTemplate.Replace("{componentType}", className).Replace("{param}", param);
+                    methodBuilder.Append(part);
+                    methodBuilder.AppendLine();
+                }
+                
+            }
+            else if (type.ToString().Contains("IUpdate"))
+            {
+                string part = EntitySystemTemplate.Replace("{lifeCycle}", "Update").Replace("{componentType}", className);
+                methodBuilder.Append(part);
+                methodBuilder.AppendLine();
+            }
+            else if (type.ToString().Contains("IDestroy"))
+            {
+                string part = EntitySystemTemplate.Replace("{lifeCycle}", "Destroy").Replace("{componentType}", className);
+                methodBuilder.Append(part);
+                methodBuilder.AppendLine();
+            }
+        }
+        
+        code = code.Replace("{EntitySystem}", methodBuilder.ToString());
         File.WriteAllText(filePath, code);
     }
 
     class ComponentClassReceiver : ISyntaxContextReceiver
     {
         // ComponentType
-        public HashSet<ClassDeclarationSyntax> CompnentTypes = new ();
-        
+        public HashSet<ClassDeclarationSyntax> CompnentTypes = new();
+
         // 已经生成的Component 和System的对应关系
         public Dictionary<String, String> Component2Systems = new();
 
@@ -152,17 +233,23 @@ public class ComponentSystemGenerator: ISourceGenerator
             {
                 return;
             }
-            
+
             var classTypeSymbol = context.SemanticModel.GetDeclaredSymbol(classDeclarationSyntax);
             if (classTypeSymbol == null)
             {
                 return;
             }
-            
+
+            // 排除FGUI
+            if (classTypeSymbol.HasAttribute(Definition.FGUITagAttribute))
+            {
+                return;
+            }
+
             // Entity | Component
             if (classTypeSymbol.HasAttribute(Definition.ChildOfAttribute) || classTypeSymbol.HasAttribute(Definition.ComponentOfAttribute))
             {
-                if(classTypeSymbol.BaseTypes().Any(x=> x.Name == "Entity"))
+                if (classTypeSymbol.BaseTypes().Any(x => x.Name == "Entity"))
                 {
                     CompnentTypes.Add(classDeclarationSyntax);
                 }
@@ -179,7 +266,6 @@ public class ComponentSystemGenerator: ISourceGenerator
                     {
                         string typeName = arg.Value.ToString().Split('.').Last();
                         Component2Systems[typeName] = classTypeSymbol.Name;
-                        
                     }
                 }
             }
